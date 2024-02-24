8b398f92b9455cf7953647177a3a23a248d103c5
Bimbo E. Villaganas <20201259@nbsc.edu.ph>
2024-02-24 09:37:53 +0800

    Added a responsive navbar to index.html file (hamburger menu)

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,8 +1,8 @@
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+document.addEventListener('DOMContentLoaded', function () {
+  const burgerMenu = document.querySelector('.burger-menu');
+  const navLinks = document.querySelector('.nav-links');
+
+  burgerMenu.addEventListener('click', function () {
+      navLinks.classList.toggle('active');
+  });
+});
